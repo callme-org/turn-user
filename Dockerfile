85040bcfacde30f8e49700d5ec79a58be1fa14be
comougi <comougi@gmail.com>
2024-05-01 14:44:32 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM gradle:8.7.0-jdk21 AS build
+COPY --chown=gradle:gradle . /home/gradle/src
+WORKDIR /home/gradle/src
+RUN gradle buildFatJar --no-daemon
+
+FROM openjdk:21
+EXPOSE 8080:8080
+RUN mkdir /app
+COPY --from=build /home/gradle/src/build/libs/*.jar /app/callme-turn-user.jar
+ENTRYPOINT ["java","-jar","/app/callme-turn-user.jar"]
---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    val kotlinVersion = libs.versions.core.kotlin.get()
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+    id("io.ktor.plugin") version libs.versions.core.ktor.get()
+}
+
+group = "com.ougi.callme"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.ougi.callme.ApplicationKt")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+
+dependencies {
+    implementation(libs.ktor.core.jvm)
+    implementation(libs.ktor.netty.jvm)
+    implementation(libs.ktor.negotiation)
+    implementation(libs.ktor.serialization)
+
+    implementation(libs.db.exposed.core)
+    implementation(libs.db.exposed.jdbc)
+    implementation(libs.db.mysql.connector)
+
+    implementation(libs.netty.logback)
+
+    implementation(libs.koin)
+}
